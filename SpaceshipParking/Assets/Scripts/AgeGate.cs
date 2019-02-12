3d42c36655894f8af6ff9ce4af63a934f8bd0296
Ben Tarrant <ben.g.tarrant@gmail.com>
2019-02-12 16:41:01 +0000

    Analytics and Tutorial
    ADDED: Analytic Services and several test custom events
    
    ADDED: preliminary background for tutorial level

---
+++ SpaceshipParking/Assets/Scripts/AgeGate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AgeGate : MonoBehaviour {
+
+    public InputField inputField;
+    int ageValue;
+
+    public Text noInput;
+    public Text tooYoung;
+
+
+    public void AgeCheck()
+    {
+
+        if (inputField.text == "")
+        {
+            Debug.Log("field is empty");
+            noInput.enabled = true;
+        }
+               
+        ageValue = int.Parse(inputField.text);
+
+
+        if (ageValue >= 18f)
+        {
+           Debug.Log("OLD ENOUGH");
+           noInput.enabled = false;
+           tooYoung.enabled = false;
+            SceneManager.LoadScene(0);
+        }
+
+        else
+        {
+            Debug.Log("TOO YOUNG");
+            noInput.enabled = false;
+            tooYoung.enabled = true;
+        }
+    }
+
+
+}
